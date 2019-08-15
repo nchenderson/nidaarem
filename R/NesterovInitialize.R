58176555f5f177c525be85942136a1b043e3467e
nchenderson <nhenders@stat.wisc.edu>
2019-08-15 14:26:13 -0500

    added first working version of NesterovInitialize

--- R/NesterovInitialize.R
+++ R/NesterovInitialize.R
@@ -1,5 +1,14 @@
 NesterovInitialize <-  function(par, fixptfn, objfn=NULL, test="monotone", ...,
                                 control=list(maxiter=500)) {
+    control.default <- list(maxiter=500)
+    namc <- names(control)
+    if (!all(namc %in% names(control.default))) {
+       stop("unknown names in control: ", namc[!(namc %in% names(control.default))])
+    }
+    control <- modifyList(control.default, control)
+    maxiter <- control$maxiter
+    
+    
     pp <- length(par)
     conv <- TRUE
     beta.vecy <- beta.old <- par
@@ -10,19 +19,19 @@ NesterovInitialize <-  function(par, fixptfn, objfn=NULL, test="monotone", ...,
         beta.new <- fixptfn(beta.vecy, ...) ## x_k, y_k = beta.vecy
         oval <- objfn(beta.new, ...)
         if(test=="monotone") {
-           done <- oval < objfn.val
+           done <- oval < objfn.val[k]
         } else if(test == "gradient") {
            done <- sum((beta.vecy - beta.new)*(beta.new - beta.old)) > 0 
         }        
         if(done) {
             break
-        } else {
-           ## Update tseq
-           alpha.new <- 1/2 + sqrt(1 + 4*alpha*alpha)/2
-           mom <- (alpha - 1)/alpha.new
-           alpha <- alpha.new
-           beta.vecy <- beta.new + mom*(beta.new - beta.old)
-        }
+        } 
+        ## Update tseq
+        alpha.new <- 1/2 + sqrt(1 + 4*alpha*alpha)/2
+        mom <- (alpha - 1)/alpha.new
+        alpha <- alpha.new
+        
+        beta.vecy <- beta.new + mom*(beta.new - beta.old)
         objfn.val[k+1] <- objfn(beta.new, ...)
         beta.old <- beta.new
     }
