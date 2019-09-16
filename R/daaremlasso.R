f35fe6c3945e4524646dd5374ae223f6c7a9a5fc
Nicholas Henderson <nchender@sph-190157-bio.ddns.sph.umich.edu>
2019-09-16 16:43:38 -0400

    Initial commit of main daarem.lasso function

---
+++ R/daaremlasso.R
@@ -0,0 +1,62 @@
+daarem.lasso <- function(par, X, y, lambda, stplngth=NULL,
+                         family = c("gaussian", "binomial"), control=list()) {
+
+  control.default <- list(maxiter=2000, order=10, tol=1.e-08, mon.tol=1, cycl.mon.tol=0.0, 
+                          objfn.check=TRUE, kappa=25, alpha=1.2)
+  namc <- names(control)
+  if (!all(namc %in% names(control.default))) {
+    stop("unknown names in control: ", namc[!(namc %in% names(control.default))])
+  }
+  control <- modifyList(control.default, control)
+
+  family = match.arg(family)
+
+  maxiter <- control$maxiter
+  tol <- control$tol
+  mon.tol <- control$mon.tol  ## monotone tolerance
+  cycl.mon.tol <- control$cycl.mon.tol
+  a1 <- control$alpha
+  kappa <- control$kappa
+
+  num.params <- length(par)
+  nlag <- min(control$order, ceiling(num.params/2))
+
+  lasso.pen <- lambda
+  Fdiff <- Xdiff <- matrix(0.0, nrow=num.params, ncol=nlag)
+  obj_funvals <- rep(NA, maxiter + 2)
+
+  ### Compute X'y and X'X and y'y
+#  if(family=="gaussian") {
+#     XtX <- crossprod(X, X)
+#     yty <- sum(y*y)
+     ## maybe modify code so we don't have to store XtX?
+#  }
+#  Xty <- crossprod(X, y)
+
+  ### Compute step length
+  if(is.null(stplngth) & family=="gaussian") {
+      stplngth <- FindStepLengthGaussian(X, y, lambda)
+      ## Migth be better to modify FindStepLengthGaussian so that
+      ## it takes XtX as an argument?
+  } else if(is.null(stplngth) & family=="binomial") {
+      stplngth <- FindStepLengthBinomial(X, y, lambda)
+  }
+
+  if(family=="gaussian" & control$objfn.check) {
+      base_fn <- ifelse(n > p, "gauss_ngtp", "gauss_pgtn")
+  } else if(family=="gaussian" & !control$objfn.check) {
+      base_fn <- ifelse(n > p, "gauss_ngtp2", "gauss_pgtn2")
+  } else if(family=="binomial" & control$objfn.check) {
+      base_fn <- "binomial_b"
+  } else if(family == "binomial" & !control$objfn.check) {
+      base_fn <- "binomial_b2"
+  }
+  ans <- switch(base_fn, gauss_ngtp = daarem_lasso_gaussian_ngtp(par, X, y, lambda, stplngth, nlag, a1, kappa, maxiter, tol, mon.tol, cycl.mon.tol),
+         gauss_pgtn = daarem_lasso_gaussian_pgtn(par, X, y, lambda, stplngth, nlag, a1, kappa, maxiter, tol, mon.tol, cycl.mon.tol),
+         gauss_ngtp2 = daarem_lasso_gaussian_ngtp2(par, X, y, lambda, stplngth, nlag, a1, kappa, maxiter, tol, mon.tol, cycl.mon.tol),
+         gauss_pgtn2 = daarem_lasso_gaussian_pgtn2(par, X, y, lambda, stplngth, nlag, a1, kappa, maxiter, tol, mon.tol, cycl.mon.tol),
+         binomial_b = daarem_lasso_binomial(par, X, y, lambda, stplngth, nlag, a1, kappa, maxiter, tol, mon.tol, cycl.mon.tol),
+         binomial_b2 = daarem_lasso_binomial2(par, X, y, lambda, stplngth, nlag, a1, kappa, maxiter, tol, mon.tol, cycl.mon.tol))
+  return(ans)
+}
+
