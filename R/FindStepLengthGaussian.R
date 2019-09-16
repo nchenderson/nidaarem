48b30dae91e0195d5b02a69b1c048475bfd693a7
Nicholas Henderson <nchender@sph-190157-bio.ddns.sph.umich.edu>
2019-09-16 16:46:10 -0400

    initial commit for finding the steplength in the Gaussian regression problem

---
+++ R/FindStepLengthGaussian.R
@@ -0,0 +1,38 @@
+FindStepLengthGaussian <- function(X, y, lam, pmax=10000, nmin=1000) {
+  ### Function for finding the steplength for Anderson
+  p <- ncol(X)
+  n <- nrow(X)
+  if(n <= nmin) {
+    Lmax <- norm(X, "2")^2
+    accurate <- TRUE
+  } else if (n > nmin & p <= pmax){
+    XtX <- crossprod(X)
+    Lmax <- eigs_sym(XtX, 1, retvec=FALSE)$values
+    accurate <- TRUE
+  } else if (p > pmax & p <= n) {
+    XtX <- crossprod(X)
+    Lmax <- max(rowSums(abs(XtXt)))
+    accurate <- FALSE
+  } else if (p > pmax & p > n) {
+    XtXt <- tcrossprod(X)
+    Lmax <- max(rowSums(abs(XtXt)))
+    accurate <- FALSE
+  }
+  if (accurate){
+    step <- 2/Lmax
+  } else {
+    beta.init <- rep(0, p)
+    step <- 1/Lmax
+    k <- 1
+    lchg <- 1
+    while (lchg > 0) {
+      k <- 2*k
+      lold <- LassoObj(beta.init, X, y, lam, k*step)
+      lnew <- LassoObj(GDLassostep(beta.init, X, y, lam, k*step), X, y, lam, k*step)
+      lchg <- lnew - lold
+    }
+    k <- max(2, k/4)
+    step <- k/Lmax
+  }
+  return(step)
+}
