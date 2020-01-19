d4903ceee84c98a5414feb148e8058ba1c4df2bc
Nicholas Henderson <nchender@sph-190157-bio.ddns.sph.umich.edu>
2020-01-19 00:42:41 -0500

    updated to handle alternating monotonicity

--- R/sub_daarem_lasso_gaussian_pgtn.R
+++ R/sub_daarem_lasso_gaussian_pgtn.R
@@ -1,5 +1,6 @@
 sub_daarem_lasso_gaussian_pgtn <- function(par, X, y, lambda, stplngth, nlag, a1, kappa, 
-                                           maxiter, tol, mon.tol, cycl.mon.tol, sub.size, sub.type="random") {
+                                           maxiter, tol, mtol, cycl.mon.tol, sub.size, sub.type) {
+
     num.params <- ncol(X)
     lasso.pen <- lambda
     Fdiff <- Xdiff <- matrix(0.0, nrow=num.params, ncol=nlag)
@@ -22,8 +23,6 @@ sub_daarem_lasso_gaussian_pgtn <- function(par, X, y, lambda, stplngth, nlag, a1
     shrink.count <- 0
     shrink.target <- 1/(1 + a1^kappa)
   
- 
-  
     lambda.ridge <- 100000
     r.penalty <- 0
     conv <- TRUE
@@ -32,26 +31,27 @@ sub_daarem_lasso_gaussian_pgtn <- function(par, X, y, lambda, stplngth, nlag, a1
     possible.index <- 1:num.params
     x.aa <- f.aa <- double(num.params)
     ind <- 1:num.params
+    if(length(mtol)==2) {
+      mon.tol <- mtol[1]
+    } else if(length(mtol)==1) {
+      mon.tol <- mtol
+    }
     while(k < maxiter) {
         count <- count + 1
 
-       # tmpf <- SoftThresh_ind(xnew + stplngth*(Xty - crossprod(X,X%*%xnew)), lambda=lambda*stplngth) 
-       # fnew <- tmpf$beta.hat - xnew
         fnew <- SoftThresh(xnew + stplngth*(Xty - crossprod(X,X%*%xnew)), lambda=lambda*stplngth) - xnew
         ss.resids <- sqrt(crossprod(fnew))
-
         
         if(ss.resids < tol & count==nlag) break
 
         np <- count
-        if (count==1 & k > 1 & sub.type=="random"){
+        if (count==1 & k > 1 & sub.type=="threshold"){
              x.aa <- f.aa <- double(num.params)
              ff.abs <- rowSums(abs(Fdiff))
              xx.abs <- rowSums(abs(Xdiff))
              ind1 <- which( ff.abs > .001*mean(ff.abs))
              ind2 <- which( xx.abs > .001*mean(xx.abs))
              ind <- union(ind1, ind2)
-             #print(c(length(ind1), length(ind2), length(ind)))
         } else if (count==1 & k > 20 & sub.type=="betas") {
              x.aa <- f.aa <- double(num.params)
              b.abs <- abs(fnew + xnew)
@@ -63,9 +63,7 @@ sub_daarem_lasso_gaussian_pgtn <- function(par, X, y, lambda, stplngth, nlag, a1
         Ftmp <- matrix(Fdiff[ind,1:np], nrow=length(ind), ncol=np)
         Xtmp <- matrix(Xdiff[ind,1:np], nrow=length(ind), ncol=np)
         fnew.tmp <- fnew[ind]
-       # Ftmp.full <- matrix(Fdiff[,1:np], nrow=length(fnew), ncol=np)
-       # Xtmp.full <- matrix(Xdiff[,1:np], nrow=length(fnew), ncol=np)
-
+     
         tmp <- La.svd(Ftmp)
         dvec <- tmp$d
         uy <- crossprod(tmp$u, fnew.tmp)
@@ -75,7 +73,6 @@ sub_daarem_lasso_gaussian_pgtn <- function(par, X, y, lambda, stplngth, nlag, a1
         min.d <- min(tmp$d)
         cond.number <- ifelse(max.d==min.d, 1, max.d/min.d)  ## to take care of cases where max.d=min.d=0
         if(cond.number > 1e10) {
-            print('large condition number')
             shrink.count <- shrink.count - 2
         }
 
@@ -88,68 +85,48 @@ sub_daarem_lasso_gaussian_pgtn <- function(par, X, y, lambda, stplngth, nlag, a1
         dd <- (dvec*uy)/(dvec^2 + lambda.ridge)
         gamma_vec <- crossprod(tmp$vt, dd)
 
-        if(class(gamma_vec) != "try-error"){
-        
-             x.aa[ind] <- Xtmp%*%gamma_vec
-             f.aa[ind] <- Ftmp%*%gamma_vec
+        x.aa[ind] <- Xtmp%*%gamma_vec
+        f.aa[ind] <- Ftmp%*%gamma_vec
 
-             xbar <- xnew - x.aa
-             fbar <- fnew - f.aa
+        xbar <- xnew - x.aa
+        fbar <- fnew - f.aa
 
-             x.propose <- xbar + fbar
-             new.objective.val <- try(LassoObjFn_pgtn(x.propose, X, Xty, yty, lasso.pen), silent=TRUE)
-     
-             obj.evals <- obj.evals + 1
+        x.propose <- xbar + fbar
+        new.objective.val <- try(LassoObjFn_pgtn(x.propose, X, Xty, yty, lasso.pen), silent=TRUE)
+        obj.evals <- obj.evals + 1
 
-             if(class(new.objective.val) != "try-error" & !is.na(obj_funvals[k+1]) &
-                !is.nan(new.objective.val)) {
-                 if(new.objective.val >= obj_funvals[k+1] - mon.tol) {
-                 ## Increase delta
-                    obj_funvals[k+2] <- new.objective.val
-                    fold <- fnew
-                    xold <- xnew
-
-                    xnew <- x.propose
-                    shrink.count <- shrink.count + 1
-                 } else {
+        if(class(new.objective.val) != "try-error" & !is.na(obj_funvals[k+1]) &
+           !is.nan(new.objective.val)) {
+              if(new.objective.val >= obj_funvals[k+1] - mon.tol) {
+                ## Increase delta
+                  obj_funvals[k+2] <- new.objective.val
+                  fold <- fnew
+                  xold <- xnew
+                  xnew <- x.propose
+                  shrink.count <- shrink.count + 1
+              } else {
                  ## Keep delta the same
-                    fold <- fnew
-                    xold <- xnew
-
-                    xnew <- fold + xold
+                  fold <- fnew
+                  xold <- xnew
+                  xnew <- fold + xold
                     
-                    ### Do we need to re-compute everything if we fall back?
-                    obj_funvals[k+2] <- LassoObjFn_pgtn(xnew, X, Xty, yty, lasso.pen)
-                    obj.evals <- obj.evals + 1
+                  ### Do we need to re-compute everything if we fall back?
+                  obj_funvals[k+2] <- LassoObjFn_pgtn(xnew, X, Xty, yty, lasso.pen)
+                  obj.evals <- obj.evals + 1
                     #num.em <- num.em + 1
-                 }
-             } else {
-                 ## Keep delta the same
-                 fold <- fnew
-                 xold <- xnew
+              }
+         } else {
+              ## Keep delta the same
+              fold <- fnew
+              xold <- xnew
+              xnew <- fold + xold
 
-                 xnew <- fold + xold
-
-                 obj_funvals[k+2] <- LassoObjFn_pgtn(xnew, X, Xty, yty, lasso.pen)  ### need to add ngtp here?
-       
-                 obj.evals <- obj.evals + 1
-                 count <- 0
+              obj_funvals[k+2] <- LassoObjFn_pgtn(xnew, X, Xty, yty, lasso.pen)  ### need to add ngtp here?
+              obj.evals <- obj.evals + 1
+              count <- 0
                  #num.em <- num.em + 1
-            }
-       } else {
-            ## Keep delta the same
-            fold <- fnew
-            xold <- xnew
-
-            xnew <- fold + xold
-      
-            obj_funvals[k+2] <- LassoObjFn_pgtn(xnew, X, Xty, yty, lasso.pen)
-     
-            obj.evals <- obj.evals + 1
-            count <- 0
-            #num.em <- num.em + 1
-       }
-       if(count==nlag) {
+        }
+        if(count==nlag) {
             count <- 0
             ## restart count
             ## make comparison here l.star vs. obj_funvals[k+2]
@@ -158,9 +135,12 @@ sub_daarem_lasso_gaussian_pgtn <- function(par, X, y, lambda, stplngth, nlag, a1
                shrink.count <- max(shrink.count - nlag, -2*kappa)
             }
             ell.star <- obj_funvals[k+2]
-       }
-       shrink.target <-  1/(1 + a1^(kappa - shrink.count))
-       k <- k+1
+            if(length(mtol)==2) {
+              mon.tol= ifelse(mon.tol==mtol[1], mtol[2], mtol[1])
+            } 
+        }
+        shrink.target <-  1/(1 + a1^(kappa - shrink.count))
+        k <- k+1
     }
     obj_funvals <- obj_funvals[!is.na(obj_funvals)]
     value.obj <- LassoObjFn_pgtn(xnew, X, Xty, yty, lasso.pen)
