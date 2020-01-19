d4a3aedd5ac56dfa1af33ab2c9467d98cd2233ea
Nicholas Henderson <nchender@sph-190157-bio.ddns.sph.umich.edu>
2020-01-18 22:42:36 -0500

    changed names of arguments

--- R/MatrixCompleteUpdate.R
+++ R/MatrixCompleteUpdate.R
@@ -1,10 +1,11 @@
-MatrixCompleteUpdate <- function(par, lambda, Y, PY, ind) {
+MatrixCompleteUpdate <- function(par, lambda, A, PA, ind) {
+  ## ind is the vector indicating the observed indices
   tt <- 1
-  B <- matrix(par, nrow=nrow(PY), ncol=ncol(PY))
+  B <- matrix(par, nrow=nrow(PA), ncol=ncol(PA))
   PB <- B
   PB[-ind] <- 0.0
   
-  Atmp <- B + tt*(PY - PB)
+  Atmp <- B + tt*(PA - PB)
   ans <- svd_soft_thresh(Atmp, lambda*tt)
   return(c(ans))
 }
